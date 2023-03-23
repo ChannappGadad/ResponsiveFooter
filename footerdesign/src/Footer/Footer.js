589d879c241cce6c27d33aece7e21f349a39ab15
ChannappGadad <118685319+ChannappGadad@users.noreply.github.com>
2023-03-23 22:35:02 +0530

    added footer and design

--- footerdesign/src/Footer/Footer.js
+++ footerdesign/src/Footer/Footer.js
@@ -1,11 +1,41 @@
 import React from "react";
+import "./Footer.css";
+import { FaFacebook, FaTwitter, FaInstagram, FaYoutube } from "react-icons/fa";
 
-function footer() {
+function Footer() {
   return (
-    <div className= "footer">
-     Hello
+    <div className= "Footer">
+      <div className="sub_footer">
+        <div>
+          <h4>NetSkill</h4>
+          <a href="/" className="SocialMedia">{<FaFacebook/>}</a>
+          <FaTwitter className="SocialMedia" />
+          <FaInstagram className="SocialMedia" />
+          <FaYoutube className="SocialMedia" />
+        </div>
+        <div>
+          <ul className="undefinedList">
+            <h4>Below items</h4>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Service</a>
+            </li>          
+            <li>
+              <a href="#">About</a>
+            </li>                  
+            <li>
+              <a href="#">Terms</a>
+            </li>          
+            <li>
+              <a href="#">Privacy policy</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default footer;
+export default Footer;
