31756779709789fd3d4ab44b458784b3389ef1d2
ChannappGadad <118685319+ChannappGadad@users.noreply.github.com>
2023-03-23 12:26:59 +0530

    react project with footer folder

---
+++ footerdesign/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Footer from './Footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h3>Hello</h3>
+      </header>
+      <footer>
+        <Footer/>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ footerdesign/src/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function footer() {
+  return (
+    <div className= "footer">
+     Hello
+    </div>
+  );
+}
+
+export default footer;
